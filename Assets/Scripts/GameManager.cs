80f6bcce5ebc6b21545f2d6e0a8227e868c948f0
erensglm <101216457+erensglm@users.noreply.github.com>
2024-08-28 17:03:23 +0300

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     public int MaxNumberOfShots = 3;
+    [SerializeField] private float _secondsToWaitBeforeDeathCheck = 3f;
 
     private int _usedNumberOfShots;
+
+    private IconHandler _iconHandler;
+
+    private List<Baddie> _baddies = new List<Baddie>();
 
     private void Awake()
     {
@@ -16,27 +22,80 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
-        else
+
+        _iconHandler = GameObject.FindObjectOfType<IconHandler>();
+
+        Baddie[] foundBaddies = FindObjectsOfType<Baddie>();
+        for (int i = 0; i < foundBaddies.Length; i++)
         {
-            Destroy(this);
+            _baddies.Add(foundBaddies[i]);
         }
     }
 
     public void UseShot()
     {
         _usedNumberOfShots++;
+        _iconHandler.UseShot(_usedNumberOfShots);
+
+        CheckForLastShot();
     }
 
     public bool HasEnoughShots()
     {
-        if (_usedNumberOfShots < MaxNumberOfShots)
+        return _usedNumberOfShots < MaxNumberOfShots;
+    }
+
+    public void CheckForLastShot()
+    {
+        if (_usedNumberOfShots == MaxNumberOfShots)
         {
-            return true;
-        }
-        else
-        {
-            return false;
+            // Eðer IconHandler scriptinde 'ShowLastShot' metodu yoksa bu kod çalýþmaz. Yorum satýrýna aldým.
+            // _iconHandler.ShowLastShot();
+
+            StartCoroutine(CheckAfterWaitTime());
         }
     }
 
+
+    private IEnumerator CheckAfterWaitTime()
+    {
+        yield return new WaitForSeconds(_secondsToWaitBeforeDeathCheck);
+
+        if (_baddies.Count == 0)
+        {
+            WinGame();
+        }
+
+        else
+        {
+            LoseGame();
+        }
+
+    }
+
+    public void RemoveBaddie(Baddie baddie)
+    {
+        _baddies.Remove(baddie);
+        CheckForAllDeadBaddies();
+    }
+
+    private void CheckForAllDeadBaddies()
+    {
+        if (_baddies.Count == 0)
+        {
+            WinGame();
+        }
+    }
+    #region Win/Lose
+
+    private void WinGame()
+    {
+        UnityEngine.Debug.Log("You win!");
+    }
+
+    private void LoseGame()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    #endregion
 }
